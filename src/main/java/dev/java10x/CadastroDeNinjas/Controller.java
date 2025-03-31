38adeee32af8527d2d3ca3679111ffa5a81db108
Lucas Azevedo <lucas.shirabayashi@gmail.com>
2025-04-01 00:30:31 +0100

    feat: add first get route

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,17 @@
+package dev.java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// usando anotation para avisar ao Sring que isso eh uma API Rest de Contolador
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Essa eh minha primeira mensagem!";
+    }
+
+}
